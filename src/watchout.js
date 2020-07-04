db22142b61cc730a6dbe728769b46e05ed41a9bf
Michael Brecht <brecht.michael.b@gmail.com>
2020-07-04 09:21:49 -0600

    Created player class and added it to the DOM

--- src/watchout.js
+++ src/watchout.js
@@ -13,6 +13,9 @@ class Watchout {
         this.maxEnemies = 10;
         this.enemies = this.generateEnemies();
 
+        // Generate Player, and set position to center of screen
+        this.player = new Player(this.width / 2, this.height / 2);
+
         // Start Game
         this.run();
     }
@@ -72,6 +75,35 @@ class Enemy {
 
     updatePosition() {
         d3.select(`#enemy${this.id}`)
+            .style('position', 'absolute')
+            .style('left', `${this.x - 50}`)
+            .style('top', `${this.y - 50}`);
+    }
+}
+
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.createElement();
+        this.updatePosition();
+    }
+
+    createElement() {
+        d3.select('body')
+            .append('svg')
+            .attr('id', 'player')
+            .attr('width', 50)
+            .attr('height', 50)
+            .append('circle')
+            .attr('cx', 25)
+            .attr('cy', 25)
+            .attr('r', 25)
+            .style('fill', 'orange');
+    }
+
+    updatePosition() {
+        d3.select(`#player`)
             .style('position', 'absolute')
             .style('left', `${this.x - 50}`)
             .style('top', `${this.y - 50}`);
