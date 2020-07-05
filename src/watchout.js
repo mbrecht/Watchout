0482509b3b901d78ec21796e8cd82edb37375b12
Michael Brecht <brecht.michael.b@gmail.com>
2020-07-04 20:06:57 -0600

    Improved readability

--- src/watchout.js
+++ src/watchout.js
@@ -1,10 +1,5 @@
 class Watchout {
     constructor() {
-        this.init();
-    }
-
-    // Initialize Game
-    init() {
         // Utilities
         this.running = true;
         this.date = new Date();
@@ -35,30 +30,42 @@ class Watchout {
     }
 
     checkCollisions() {
-        for(let i = 0; i < this.maxEnemies; i++) {
-            let radius1 = 25;
-            let radius2 = this.enemies[i].radius;
-            let x1 = this.player.x - (radius1 * 1.5);
-            let x2 = this.enemies[i].pos.x;
-            let y1 = this.player.y - (radius1 * 1.5);
-            let y2 = this.enemies[i].pos.y;
-            
-            if ( Math.sqrt( ( x2-x1 ) * ( x2-x1 )  + ( y2-y1 ) * ( y2-y1 ) ) < ( radius1 + radius2 ) ) 
-            {
-                this.collisionDetected = true;
-                break;
-            } else {
-                this.collisionDetected = false;
-            }
-        }
+        let playerCircle = {
+            x: this.player.x,
+            y: this.player.y,
+            r: this.playerRadius
+        };
+        
+        let enemyCircle = {};
+
+        this.enemies.forEach(enemy => {
+            enemyCircle.x = enemy.pos.x;
+            enemyCircle.y = enemy.pos.y;
+            enemyCircle.r = enemy.radius;
+            if(this.circleCollision({...playerCircle}, {...enemyCircle})) this.collisionDetected = true;
+        });
+    }
+
+    circleCollision(circle1, circle2) {
+        circle1.x = circle1.x - (circle1.r * 1.5);
+        circle1.y = circle1.y - (circle1.r * 1.5);
+
+        let dx = circle1.x - circle2.x;
+        let dy = circle1.y - circle2.y;
+
+        let distance =  Math.sqrt(dx * dx + dy * dy);
+        
+        if ( distance < ( circle1.r + circle2.r ) ) 
+        {
+            return true;
+        } 
+        return false;
     }
 
     update() {
         this.date = new Date();
         let currentTime = this.date.getTime();
         let delta = ((currentTime - this.time) / 1000);
-
-
 
         d3.select('#time')
             .text(`Time: ${this.time}`);
@@ -93,16 +100,11 @@ class Watchout {
     }
 
     updateEnemies(delta) {
-        for(let i = 0; i < this.maxEnemies; i++) {
-            this.enemies[i].move(delta);
-        }
+        this.enemies.forEach(enemy => enemy.move(delta));
     }
 
     updateEnemyPos() {
-        for(let i = 0; i < this.maxEnemies; i++) {
-            let pos = this.randomPos();
-            this.enemies[i].updatePosition(pos);
-        }
+        this.enemies.forEach(enemy => enemy.updatePosition(this.randomPos()));
     }
 
     updateScore() {
@@ -118,10 +120,6 @@ class Watchout {
         pos.y = Math.floor(Math.random() * (this.height - 30));
 
         return pos;
-    }
-
-    // Shamelessly stolen from MDN
-    circleCollision(circle1, circle2) {
     }
 }
 
@@ -165,8 +163,8 @@ class Enemy {
         this.nextPos = {...pos};
     }
 
-    move(delta) {
-        if(delta === 1) {
+    move(delta) { 
+        if (delta === 1) {
             this.pos = {...this.lastPos}
         } else {
             this.pos.x = Math.floor(this.lerp(this.lastPos.x, this.nextPos.x, delta));
